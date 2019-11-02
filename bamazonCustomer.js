eceb8dd63cd96dd81fe9712fe79d148ad7b7cceb
Joshua Einsel <Joshua.einsel@Live.com>
2019-11-02 12:44:10 -0700

    Added update functionality for when an item is purchased.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,7 +21,8 @@ connection.connect(function (err) {
     listItems();
 });
 
-
+var quantity = 0;
+var price = 0;
 
 function listItems() {
     connection.query(`SELECT * FROM products`, function (error, results) {
@@ -30,7 +31,7 @@ function listItems() {
 
         for (let i = 0; i < results.length; i++) {
             const element = results[i];
-            console.log(`ID: ${element.item_id} - ${element.department} - $${element.price} -quantity:${element.quantity}`)
+            console.log(`ID: ${element.item_id} - Item Name:${element.name} - ${element.department} - $${element.price} -quantity:${element.quantity}`)
         }
         inquirer.prompt([
             {
@@ -41,15 +42,41 @@ function listItems() {
         ]).then((answers) => {
             var userChoice = answers.id
             console.log(userChoice)
-            if (results[userChoice].quantity === null) {
-                console.log(userInput[userChoice].quantity)
-                console.log("You cannot purchase that item")
-            }
-            else { console.log("There is enough quantity!") }
-
+            connection.query(`SELECT * FROM products WHERE item_id ="${userChoice}"`, function (error, results) {
+                if (error) throw err;
+                if (results[0].quantity === 0) {
+                    console.log("Error, item quantity is 0 " + results[0].quantity)
+                    console.log("That item is sold out, please try again later.")
+                } else if (results[0].quantity > 0) {
+                    console.log(`Your item information - ID: ${results[0].item_id} name: ${results[0].name} Department: ${results[0].department} Price: $${results[0].price} dollars`);
+                }
+                quantity = results[0].quantity;
+                price = results[0].price;
+            })
+            inquirer.prompt([
+                {
+                    name: "choice",
+                    type: "input",
+                    message: "How many do you want to buy?"
+                }
+            ]).then((answers) => {
+                var sum = quantity - answers.choice;
+                var total = price * answers.choice;
+                if (sum <= 0) {
+                    console.log(`There is not enough quantity`)
+                } else {
+                    connection.query(`UPDATE products SET quantity = ${sum} WHERE item_id ="${userChoice}"`, function (error, results) {
+                        if (error) {
+                            throw error;
+                        }
+                        console.log(results)
+                        console.log(`Your price is $${total} dollars`)
+                    })
+                }
+            })
         })
             .catch((error) => {
-                console.log("got error "+error);
+                console.log("got error " + error);
             })
     }
     )
